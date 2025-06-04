7a969ccbfb995bc38745cdfb09a550ddabfc1631
Xavier Godart <xgodart@deezer.com>
2025-06-04 17:27:41 +0200

    add map math function

--- examples/scale/scale.js
+++ examples/scale/scale.js
@@ -8,6 +8,6 @@ function draw() {
   c.stroke(" ", "255", "0");
   c.circle(0, 0, 5);
   c.circle(10, 10, 5);
-  c.scale((Math.sin(c.framecount * 0.1) / 2 + 0.5) * 10);
+  c.scale(c.map(Math.sin(c.framecount * 0.1), -1, 1, 0, 10));
   c.rotate(c.framecount * 0.2);
 }
--- math.go
+++ math.go
@@ -7,3 +7,7 @@ func (c *Canvas) Distance(x1, y1, x2, y2 int) float64 {
 	dy := y2 - y1
 	return math.Sqrt(float64(dx*dx + dy*dy))
 }
+
+func (c *Canvas) Map(value, inputStart, inputEnd, outputStart, outputEnd float64) float64 {
+	return outputStart + ((outputEnd-outputStart)/(inputEnd-inputStart))*(value-inputStart)
+}
