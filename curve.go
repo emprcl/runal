df59f662bfe0a77ad7ed75ca6fc88409be347cd2
Xavier Godart <xgodart@deezer.com>
2025-06-13 13:49:51 +0200

    add bezier curves

---
+++ curve.go
@@ -0,0 +1,24 @@
+package runal
+
+import "math"
+
+func (c *Canvas) Bezier(x1, y1, x2, y2, x3, y3, x4, y4 int) {
+	steps := 50
+
+	for i := 0; i <= steps; i++ {
+		t := float64(i) / float64(steps)
+		u := 1 - t
+
+		x := u*u*u*float64(x1) +
+			3*u*u*t*float64(x2) +
+			3*u*t*t*float64(x3) +
+			t*t*t*float64(x4)
+
+		y := u*u*u*float64(y1) +
+			3*u*u*t*float64(y2) +
+			3*u*t*t*float64(y3) +
+			t*t*t*float64(y4)
+
+		c.Point(int(math.Round(x)), int(math.Round(y)))
+	}
+}
---
+++ examples/bezier/bezier.js
@@ -0,0 +1,9 @@
+function setup() {
+  c.cellPadding(" ");
+}
+
+function draw() {
+  c.clear();
+  c.stroke("0", "#ffffff", "#000000");
+  c.bezier(10, 10, 20, 0, 30, 20, 40, 10);
+}
