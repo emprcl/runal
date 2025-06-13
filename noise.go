aa600cd581eefff7a55463529a1f3cfc8c104e01
Xavier Godart <xgodart@deezer.com>
2025-06-13 16:30:45 +0200

    fix noise range

--- noise.go
+++ noise.go
@@ -21,9 +21,9 @@ func (c *Canvas) NoiseSeed(seed int64) {
 }
 
 func (c *Canvas) Noise1D(x float64) float64 {
-	return c.noise.Noise1D(x)
+	return c.noise.Noise1D(x)/2 + 0.5
 }
 
 func (c *Canvas) Noise2D(x, y float64) float64 {
-	return c.noise.Noise2D(x, y)
+	return c.noise.Noise2D(x, y)/2 + 0.5
 }
