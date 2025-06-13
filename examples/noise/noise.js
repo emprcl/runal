d14ccfc69746ba3d565f7969482a552bd350e9e4
Xavier Godart <xgodart@deezer.com>
2025-06-13 16:32:17 +0200

    fix noise example

--- examples/noise/noise.js
+++ examples/noise/noise.js
@@ -6,7 +6,7 @@ function setup() {
 function draw() {
   for (let i = 0; i < c.width; i++) {
     for (let j = 0; j < c.height; j++) {
-      let color = c.map(c.noise2D(i * 0.05, j * 0.05), 0, 1, 232, 255);
+      let color = c.map(c.noise2D(i * 0.005, j * 0.005), 0, 1, 0, 255);
       c.stroke("0", color, "#000000");
       c.point(i, j);
     }
