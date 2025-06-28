9bdfea1dc44a413649de8e0181d7e89a3df9957d
Xavier Godart <xavier.godart64@gmail.com>
2025-06-28 13:30:43 +0200

    update noiseloop example

--- examples/animation-loop/noiseloop.js
+++ examples/animation-loop/noiseloop.js
@@ -1,21 +1,26 @@
 let duration = 3;
 let radius = 4;
+let seed1 = Date.now();
+let seed2 = seed1 + 1000;
 
 function setup(c) {
-  c.cellPadding(".");
+  c.cellPaddingDouble();
 }
 
 function draw(c) {
   c.clear();
 
   let theta = c.loopAngle(duration);
+  c.noiseSeed(seed1);
   let noise = c.noiseLoop(theta, 1);
-
+  c.noiseSeed(seed2);
+  let noise2 = c.noiseLoop(theta, 1);
   let x = c.map(noise, 0, 1, radius, c.width - radius);
-  let y = c.map(Math.sin(theta), -1, 1, radius, c.height - radius);
+  let y = c.map(noise2, 0, 1, radius, c.height - radius);
   let color = c.map(noise, 0, 1, 0, 255);
+  let colorBg = c.map(noise2, 0, 1, 0, 255);
 
-  c.background("#", 3, 3);
+  c.background("#", colorBg, colorBg);
   c.stroke(" ", color, color);
   c.fill(" ", color, color);
   c.circle(x, y, radius);
@@ -23,6 +28,10 @@ function draw(c) {
 
 function onKey(c, key) {
   if (key == " ") {
-    c.noiseSeed(Date.now());
+    seed1 = Date.now();
+    seed2 = seed1 + 1000;
+  }
+  if (key == "c") {
+    c.saveCanvasToMP4("flash.mp4", duration);
   }
 }
