caacc0c82a4fa687994f7febd7304bb24d326a89
Xavier Godart <xavier.godart64@gmail.com>
2025-10-25 15:38:05 +0200

    fix debug flow

--- debug.go
+++ debug.go
@@ -15,8 +15,8 @@ func (c *Canvas) Debug(messages ...any) {
 	msg := fmt.Sprint(messages...)
 	c.debugBuffer = append(c.debugBuffer, msg)
 
-	if len(c.debugBuffer) > maxDebugBufferSize {
-		c.debugBuffer = c.debugBuffer[len(c.debugBuffer)-maxDebugBufferSize:]
+	if len(c.debugBuffer) > min(maxDebugBufferSize, c.termHeight) {
+		c.debugBuffer = c.debugBuffer[len(c.debugBuffer)-min(maxDebugBufferSize, c.termHeight):]
 	}
 }
 
